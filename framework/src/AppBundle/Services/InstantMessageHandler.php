798239488f208e6536cfe0add5ee07c648389cc5
Yasmany Cubela Medina <yasmanycm@gmail.com>
2016-05-25 14:12:05 -0400

    adding extra data to instant message push

--- framework/src/AppBundle/Services/InstantMessageHandler.php
+++ framework/src/AppBundle/Services/InstantMessageHandler.php
@@ -84,6 +84,8 @@ class InstantMessageHandler extends MessageHandler
                 foreach ($devices as $device) {
                     if ($device && array_key_exists($device->getOS(), $this->pushNotificationServices)) {
                         $extra = ['type' => 2];
+                        $extra['messageId'] = $message->getId();
+                        $extra['messageRoomId'] = $messageDependant->getRoom()->getId();
                         $extra['receivers'] = $receivers;
                         $extra['sender'] = $message->getCustomer()->getUsername();
                         $title = sprintf('New Mum from %s', $message->getCustomer()->getUsername());
