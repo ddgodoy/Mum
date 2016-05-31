779baec687018cc70b5044083b005104ce3df67f
Yasmany Cubela Medina <yasmanycm@gmail.com>
2016-05-31 16:06:20 -0400

    fixing query

--- framework/src/AppBundle/EntityRepository/MessageReceiverRepository.php
+++ framework/src/AppBundle/EntityRepository/MessageReceiverRepository.php
@@ -14,7 +14,8 @@ class MessageReceiverRepository extends EntityRepository
 {
     public function findAllByReceived(CustomerInterface $customer, $received = false)
     {
-        $exp = $this->getEntityManager()->getExpressionBuilder();
+        $exp = $this->getEntityManager()
+            ->createQueryBuilder()->expr();
         $query = $this->getEntityManager()
             ->createQueryBuilder()
             ->select('i, m, r')
@@ -25,9 +26,9 @@ class MessageReceiverRepository extends EntityRepository
             ->andWhere('r.receivers LIKE :receivers');
 
         if ($received) {
-            $query->andWhere($exp->orWhere('r.received LIKE :received')->orWhere('r.received LIKE IS NOT NULL'));
+            $query->andWhere($exp->orX('r.received LIKE :received', 'r.received LIKE IS NOT NULL'));
         } else {
-            $query->andWhere($exp->orWhere('r.received NOT LIKE :received')->orWhere('r.received IS NULL'));
+            $query->andWhere($exp->orX('r.received NOT LIKE :received', 'r.received IS NULL'));
         }
 
         return $query->setParameter('received', sprintf("%%\"%s\":true%%", $customer->getId()))
